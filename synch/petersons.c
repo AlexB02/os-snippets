f8c7880b8d434e8073b971c7285aeca4a8a936fc
AlexB.02 <alexanderdb8@gmail.com>
2022-05-14 00:58:14 +0100

    fix(petersons.c): add void to example function parameters

--- synch/petersons.c
+++ synch/petersons.c
@@ -7,7 +7,7 @@ int interested[2] = {0, 0}; // Records if a thread wants to enter CR
 void enter_critical(int thread) { 
   interested[thread] = 1; // Note interest
   int other = 1 - thread; // Other thread number
-  
+
   /* Setting the turn to other, we allow for the other thread to have access
   to the CR first, if they are already interested in the CR when 
   the condition of the while loop is checked */
@@ -22,13 +22,13 @@ void leave_critical(int thread) { interested[thread] = 0; }
 
 // Example use:
 
-void thread_0_entry() { 
+void thread_0_entry(void) { 
   enter_critical(0);
   // now has access
   leave_critical(0);
 }
 
-void thread_1_entry() { 
+void thread_1_entry(void) { 
   enter_critical(1);
   // now has access
   leave_critical(1);
